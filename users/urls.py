9278cffb4f1654bd224fd6cbcefee8dcd8168c69
Ashutosh Pradhan <ashutoshpradhan164@gmail.com>
2022-05-12 22:58:13 +0530

    loging succes using email

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
+from .views import loginUser, index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('login/', loginUser, name='login'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('<h1>Login success full </h1>')
+
+
+def loginUser(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Username Or Password is incorrect')
+
+    context = {}
+    return render(request, 'login.html', context)
+
